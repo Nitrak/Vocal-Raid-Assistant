f04432680f6412843f2f9151f08231bb3a4e51c8
nyy <nyydev@gmail.com>
2022-01-16 20:07:45 +0100

    add libdual spec support
    # Conflicts:
    #	Locales/enUS.lua
    #	VocalRaidAssistant.lua
    #	changelog.md
    #	spelllist.lua

--- Config.lua
+++ Config.lua
@@ -449,6 +449,7 @@ end
 function addon:InitConfigOptions()
     profile = addon.db.profile
     mainOptions.args.profiles = self.ACDBO:GetOptionsTable(self.db)
+    addon.LDS:EnhanceOptions(mainOptions.args.profiles, self.db)
     addon.AC:RegisterOptionsTable("VocalRaidAssistantConfig", mainOptions)
     addon.ACD:SetDefaultSize("VocalRaidAssistantConfig", 965, 650)
 end
